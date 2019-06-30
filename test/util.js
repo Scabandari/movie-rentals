263f2676e50fff3fc846c19b72c238e372e4fefc
Scabandari <ryan3nichols@gmail.com>
2019-06-30 16:35:25 -0400

    Mocha and chai framework working. Tests for Customer done with the possible exception of PATCH since that route isn't made yet

---
+++ test/util.js
@@ -0,0 +1,12 @@
+const expect = require('chai').expect;
+
+
+module.exports = {
+    // haveProperties: (model_instance, prop_list) => {  // todo not using this yet
+    //     prop_list.forEach(prop => {
+    //         expect(model_instance).to.have.property(prop);
+    //         }
+    //     );
+    // },
+};
+
